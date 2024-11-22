3fd89b1a4213ae77fed0dc0fa40023e1fd72d16c
developerMasum <developer.masum2001@gmail.com>
2024-11-22 10:50:24 +0600

    added code to main by Masum

--- client/app/(withDashboardLayout)/dashboard/page.tsx
+++ client/app/(withDashboardLayout)/dashboard/page.tsx
@@ -6,7 +6,7 @@ import Awarded from "@/app/components/home/Awarded";
 const DashboardPage = () => {
   return (
     <div>
-      <div className=" p-5 bg-gray-200 min-h-screen grid grid-cols-1 lg:grid-cols-10 gap-10">
+      <div className=" p-5 bg-gray-200 min-h-screen  grid grid-cols-1 lg:grid-cols-10 gap-10">
         {/* Dashboard Stats */}
         <div className="lg:col-span-2 flex justify-center">
           <Stats />
--- client/app/components/dashboard/AttendanceChart.tsx
+++ client/app/components/dashboard/AttendanceChart.tsx
@@ -1,23 +1,22 @@
-"use client"
+"use client";
 import dynamic from "next/dynamic";
 
-
-const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const AttendanceChart = () => {
   const chartOptions = {
     chart: {
-    id:'graph1',
-      type: 'bar',
+      id: "graph1",
+      type: "bar",
       height: 450,
       stacked: true,
-      stackType: '100%',
+      stackType: "100%",
     },
     dataLabels: {
-        formatter: (val: string) => {
-          return val + '%'
-        }
+      formatter: (val: string) => {
+        return val + "%";
       },
+    },
     // responsive: [
     //   {
     //     breakpoint: 480,
@@ -31,44 +30,57 @@ const AttendanceChart = () => {
     //   },
     // ],
     xaxis: {
-        categories: [ 'Staff', 'Internal Audit',  'Marketing', 'Accounts', 'Electrical'],
+      categories: [
+        "Staff",
+        "Internal Audit",
+        "Marketing",
+        "Accounts",
+        "Electrical",
+      ],
     },
-    yaxis:{
-        labels: {
-            formatter: (val:string) => {
-              return val + '%'
-            }
-          }
+    yaxis: {
+      labels: {
+        formatter: (val: string) => {
+          return val + "%";
+        },
+      },
     },
     fill: {
       opacity: 1,
     },
     legend: {
-      position: 'top',
+      position: "top",
       offsetX: 10,
       offsetY: 5,
     },
   };
 
   const chartSeries = [
-        {
-        name: 'Leave %',
-        data: [10, 20, 15, 10, 5 ], 
-      },
-      {
-        name: 'Present %',
-        data: [50, 30, 40, 35, 60], 
-      },
-      {
-        name: 'Absent %',
-        data: [40, 50, 45, 55, 35],
-      },
+    {
+      name: "Leave %",
+      data: [10, 20, 15, 10, 5],
+    },
+    {
+      name: "Present %",
+      data: [50, 30, 40, 35, 60],
+    },
+    {
+      name: "Absent %",
+      data: [40, 50, 45, 55, 35],
+    },
   ];
 
   return (
     <div className="bg-white py-4 lg:h-[92%]  h-[450px] rounded-2xl shadow-md">
-<h2 className="lg:text-xl text-md font-semibold border-b px-4 pb-4">Daily Attendance Statistics</h2>
-      <ApexChart options={chartOptions} series={chartSeries} type="bar" height={'92%'} />
+      <h2 className="lg:text-xl text-md font-semibold border-b px-4 pb-4">
+        Daily Attendance Statistics
+      </h2>
+      <ApexChart
+        options={chartOptions}
+        series={chartSeries}
+        type="bar"
+        height={"92%"}
+      />
     </div>
   );
 };
