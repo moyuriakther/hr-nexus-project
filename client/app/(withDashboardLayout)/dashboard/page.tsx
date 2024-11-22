9e7cd83fa80b439ffd357f2824255de8f52f09d1
Moyuri Akther <75539860+moyuriakther@users.noreply.github.com>
2024-11-22 15:35:53 +0600

    update cheanges

--- client/app/(withDashboardLayout)/dashboard/page.tsx
+++ client/app/(withDashboardLayout)/dashboard/page.tsx
@@ -11,11 +11,8 @@ const DashboardPage = () => {
   return (
   <div>
 
-      <div className='h-screen grid bg-gray-100 p-3 grid-cols-1 lg:grid-cols-12
 
-      <div className='h-screen grid bg-gray-200   p-3 grid-cols-1 lg:grid-cols-12
-
-      gap-4'>
+      <div className='min-h-screen grid bg-gray-200   p-3 grid-cols-1 lg:grid-cols-12 gap-4'>
         {/* Dashboard Stats */}
       <div className='lg:col-span-3 ' >
       <Stats/>
