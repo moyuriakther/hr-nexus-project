eda6a714feb9e14bb969dece5f1295da593603eb
developerMasum <developer.masum2001@gmail.com>
2024-11-27 13:21:07 +0600

    store and redux issue solved

--- client/app/Redux/store.ts
+++ client/app/Redux/store.ts
@@ -1,13 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sidebarReducer from "./sidebar/sidebarSlice";
+import { baseApi } from "./api/baseApi";
+
+// Combine reducers
+const reducer = {
+  sidebar: sidebarReducer,
+  [baseApi.reducerPath]: baseApi.reducer,
+};
 
 export const store = configureStore({
-  reducer: {
-    sidebar: sidebarReducer,
-  },
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
