1e9985ba60c8d001b4682de78701c1d66b15ae52
Tasmim Rahman <tasmimrahman2000@gmail.com>
2024-11-30 21:01:01 +0600

    award-api, attendance-api

--- client/app/Redux/tagTypes.ts
+++ client/app/Redux/tagTypes.ts
@@ -2,7 +2,16 @@ export enum tagTypes {
   admin = "admin",
   employee = "employee",
   department = "department",
-  subDepartment = "subDepartment"
+  subDepartment = "subDepartment",
+  attendance = "attendance",
+  award = "award",
 }
 
-export const tagTypesList = [tagTypes.admin, tagTypes.employee, tagTypes.department, tagTypes.subDepartment];
+export const tagTypesList = [
+  tagTypes.admin,
+  tagTypes.employee,
+  tagTypes.department,
+  tagTypes.subDepartment,
+  tagTypes.attendance,
+  tagTypes.award,
+];
